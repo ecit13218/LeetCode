bdadae6973277f7da378792126646ceca08ca191
zhengyao <woshizy0506>
2019-04-23 17:52:58 +0800

    重塑矩阵，给定一个矩阵，并且给定一个行，一个列，将原有矩阵转换为新的矩阵，如果不能转换，返回原有矩阵

--- src/com/zhengyao/array/Solution566.java
+++ src/com/zhengyao/array/Solution566.java
@@ -1,9 +1,10 @@
 package com.zhengyao.array;
 
 /**
- * 556
+ * Created by zhengyao on 2018/8/3
+ * DESCRIPTION:重塑矩阵，给定一个矩阵，并且给定一个行，一个列，将原有矩阵转换为新的矩阵，如果不能转换，返回原有矩阵
  */
-public class Solution {
+public class Solution566 {
     public static void main(String[] args) {
         int[][] nums=new int[][]{{1,2,3,4}};
         int[][] newNums = matrixReshape(nums, 2, 2);
@@ -14,12 +15,20 @@ public class Solution {
             System.out.println();
         }
     }
+    /**
+     * 功能描述:首先把原有矩阵先拉成一个数组，然后对这个数组进行运算，转换为r行c列的矩阵
+     * @param:
+     * @return:
+     * @auther: zhengyao3@郑瑶
+     * @date: 4/23/2019 3:12 PM
+     */
     public static int[][] matrixReshape(int[][] nums, int r, int c) {
         int length = 0;
         for (int[] len :
                 nums) {
             length += len.length;
         }
+        //如果需要的矩阵长度和现有矩阵长度不匹配，则证明无法转换矩阵，直接返回原有矩阵
         if (length != r * c) {
             return nums;
         }
