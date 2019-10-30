5a58a17787cdbd2d019737bcc836a9b7af7a598a
zhengyao <zhengyao6@foxmail.com>
2019-10-30 17:41:14 +0800

    奇偶链表

--- src/com/zhengyao/List/ListNode.java
+++ src/com/zhengyao/List/ListNode.java
@@ -6,7 +6,18 @@ package com.zhengyao.List;
  * @Description: 链表节点
  */
 public class ListNode {
-      public int val;
-      public ListNode next;
-      public ListNode(int x) { val = x; }
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    private void add(int val) {
+        ListNode node = this;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = new ListNode(val);
+    }
 }
