ac328ce42b090343090a30cead18ea5da766d183
zhengyao3 <zhengyao3@yonyou.com>
2020-03-31 23:07:00 +0800

    去除多余代码

--- src/com/zhengyao/algorithm/stack/Solution496.java
+++ src/com/zhengyao/algorithm/stack/Solution496.java
@@ -19,7 +19,6 @@ public class Solution496 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            Stack<Integer> stack = new Stack<>();
             int index = -1;
             boolean flag = false;
             for (int j = 0; j < nums2.length; j++) {
